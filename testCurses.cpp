ab8e7d9c732d52199b3eddd912b5b8a67c67d91d
Elliot Friedman <elliotfriedman3@gmail.com>
2019-01-27 15:41:00 -0800

    Added bottom and top of map

--- testCurses.cpp
+++ testCurses.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #define EMPTY     ' '
+#define LEN       400
 
 WINDOW *create_newwin(int height, int width, int starty, int startx)
 {
@@ -28,11 +29,41 @@ int moveOk(int y, int x)
 	return (testChar == EMPTY);
 }
 
+void	seedMap(int storx[LEN], int story[LEN], int yOff)
+{
+	for (int x = 0; x < LEN; x++)
+	{
+		int y = std::rand() % 10 + yOff;
+		int sx = std::rand() % LEN - 300;
+
+		if (std::rand() % 2)
+		{
+			mvaddch(y, sx, 'b');
+			storx[x] = sx;
+			story[x] = y;
+		}
+	}
+}
+
+void	winRefresh(int storx[LEN], int story[LEN], int yOff)
+{
+	for (int x = 0; x < LEN; x++)
+		{
+			mvaddch(story[x], storx[x], ' ');
+			storx[x] += 1;
+			mvaddch(story[x], storx[x], 'b');
+			if (storx[x] == COLS && x + 5 >= LEN)
+				seedMap(storx, story, yOff);
+		}
+}
+
 int main(int argc, char *argv[])
 {
 	WINDOW *my_win;
 	int startx, starty, width, height;
 	int ch;
+
+	srand(std::time(nullptr));
 
 	initscr();
 	cbreak();
@@ -49,19 +80,39 @@ int main(int argc, char *argv[])
 
 	mvaddch(starty, startx, '^');
 
+	//	nodelay(my_win, false);
 	halfdelay(1);
 	int i = 0;
-	for (int x = COLS / 2; x < COLS * 3 / 4; x++)
-	{
-		mvvline(0, x, '^', LINES);
-	}
+	//	for (int x = COLS / 2; x < COLS * 3 / 4; x++)
+	//	{
+	//		mvvline(0, x, '^', LINES);
+	//	}
+	int storx[LEN];
+	int	story[LEN];
+	/*	for (int x = 0; x < LEN; x++)
+		{
+		int y = std::rand() % 10;
+		int sx = std::rand() % LEN;
+
+		if (std::rand() % 2)
+		{
+		mvaddch(y, sx, 'b');
+		storx[x] = sx;
+		story[x] = y;
+		}
+		}
+		*/
+	timeout(1);
+	wtimeout(my_win, 1);
+	seedMap(storx, story, 0);
+	int storx1[LEN];
+	int	story1[LEN];
+	seedMap(storx1, story1, 50);
+	
 	while((ch = getch()) != KEY_F(1))
 	{
-		for (int x = COLS / 2; x < COLS * 3 / 4; x++)
-		{
-			if (std::srand(std::time(nullptr)) % 2)
-				mvvline(0, x, '^', LINES);
-		}	
+		winRefresh(storx1, story1, 50);
+		winRefresh(storx, story, 0);
 		mvaddch(starty, startx, '^');
 		move(starty, startx);
 		switch(ch)
@@ -103,4 +154,3 @@ int main(int argc, char *argv[])
 	std::cout << "ran loop " + std::to_string(i) + " times\n";
 	return 0;
 }
-
