2a7612517ddf00212a2ab0fe29b72f60b1df3be1
Elliot Friedman <elliotfriedman3@gmail.com>
2019-01-27 14:47:21 -0800

    added ncurses diplay

--- testCurses.cpp
+++ testCurses.cpp
@@ -1,83 +1,106 @@
 #include <ncurses.h>
+#include <iostream>
 
-
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
+#define EMPTY     ' '
 
 WINDOW *create_newwin(int height, int width, int starty, int startx)
-{	WINDOW *local_win;
+{
+	WINDOW *local_win;
 
 	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
-
+	box(local_win, 0 , 0);
+	wrefresh(local_win);	
 	return local_win;
 }
 
 void destroy_win(WINDOW *local_win)
 {	
-	/* box(local_win, ' ', ' '); : This won't produce the desired
-	 * result of erasing the window. It will leave it's four corners 
-	 * and so an ugly remnant of window. 
-	 */
 	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	/* The parameters taken are 
-	 * 1. win: the window on which to operate
-	 * 2. ls: character to be used for the left side of the window 
-	 * 3. rs: character to be used for the right side of the window 
-	 * 4. ts: character to be used for the top side of the window 
-	 * 5. bs: character to be used for the bottom side of the window 
-	 * 6. tl: character to be used for the top left corner of the window 
-	 * 7. tr: character to be used for the top right corner of the window 
-	 * 8. bl: character to be used for the bottom left corner of the window 
-	 * 9. br: character to be used for the bottom right corner of the window
-	 */
 	wrefresh(local_win);
 	delwin(local_win);
 }
 
+int moveOk(int y, int x)
+{
+	int testChar;
+
+	testChar = mvinch(y, x);
+	return (testChar == EMPTY);
+}
+
 int main(int argc, char *argv[])
-{	WINDOW *my_win;
+{
+	WINDOW *my_win;
 	int startx, starty, width, height;
 	int ch;
 
 	initscr();
-	cbreak();			/* Line buffering disabled, Pass on
-					 * everty thing to me 		*/
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+	cbreak();
+	keypad(stdscr, TRUE);
+	noecho();
 
-	height = 3;
-	width = 10;
-	starty = (LINES - height) / 2;	/* Calculating for a center placement */
-	startx = (COLS - width) / 2;	/* of the window		*/
-	printw("Press F1 to exit");
+	height = 1;
+	width = 1;
+	starty = (LINES - height) / 2;
+	startx = (COLS - width) / 2;
+	printw("Press F1 to exit, lines: %d, cols: %d\n", LINES, COLS);
 	refresh();
-	my_win = create_newwin(height, width, starty, startx);
+	my_win = newwin(height, width, starty, startx);
 
+	mvaddch(starty, startx, '^');
+
+	halfdelay(1);
+	int i = 0;
+	for (int x = COLS / 2; x < COLS * 3 / 4; x++)
+	{
+		mvvline(0, x, '^', LINES);
+	}
 	while((ch = getch()) != KEY_F(1))
-	{	switch(ch)
-		{	case KEY_LEFT:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, starty,--startx);
+	{
+		for (int x = COLS / 2; x < COLS * 3 / 4; x++)
+		{
+			if (std::srand(std::time(nullptr)) % 2)
+				mvvline(0, x, '^', LINES);
+		}	
+		mvaddch(starty, startx, '^');
+		move(starty, startx);
+		switch(ch)
+		{
+			case KEY_LEFT:
+				if (moveOk(starty, startx - 1))
+				{
+					mvaddch(starty, startx, ' ');
+					startx--;
+				}
 				break;
 			case KEY_RIGHT:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, starty,++startx);
+				if (moveOk(starty, startx + 1))
+				{
+					mvaddch(starty, startx, ' ');
+					startx++;
+				}
 				break;
 			case KEY_UP:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, --starty,startx);
+				if (moveOk(starty - 1, startx))
+				{
+					mvaddch(starty, startx, ' ');
+					starty--;
+				}
 				break;
 			case KEY_DOWN:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, ++starty,startx);
+				if (moveOk(starty + 1, startx))
+				{
+					mvaddch(starty, startx, ' ');
+					starty++;
+				}
 				break;	
+			default:
+				break;
 		}
+		i++;
 	}
-		
-	endwin();			/* End curses mode		  */
+	endwin();
+	std::cout << "ran loop " + std::to_string(i) + " times\n";
 	return 0;
 }
 
