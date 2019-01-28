12daec54e751ae115f84eb3c22ebe14dcb6ae133
Elliot Friedman <elliotfriedman3@gmail.com>
2019-01-27 17:28:22 -0800

    Added box for player and made borders dynamic

--- testCurses.cpp
+++ testCurses.cpp
@@ -1,3 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testCurses.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/27 17:19:10 by efriedma          #+#    #+#             */
+/*   Updated: 2019/01/27 17:25:09 by efriedma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <chrono> 
+#include <ctime> 
+
 #include <ncurses.h>
 #include <iostream>
 
@@ -31,7 +46,7 @@ int moveOk(int y, int x)
 
 void	seedMap(int storx[LEN], int story[LEN], int yOff)
 {
-	for (int x = 0; x < LEN; x++)
+	for (int x = 0; x < LEN * 2; x++)
 	{
 		int y = std::rand() % 10 + yOff;
 		int sx = std::rand() % LEN - 300;
@@ -39,8 +54,8 @@ void	seedMap(int storx[LEN], int story[LEN], int yOff)
 		if (std::rand() % 2)
 		{
 			mvaddch(y, sx, 'b');
-			storx[x] = sx;
-			story[x] = y;
+			storx[x % LEN] = sx;
+			story[x % LEN] = y;
 		}
 	}
 }
@@ -48,13 +63,13 @@ void	seedMap(int storx[LEN], int story[LEN], int yOff)
 void	winRefresh(int storx[LEN], int story[LEN], int yOff)
 {
 	for (int x = 0; x < LEN; x++)
-		{
-			mvaddch(story[x], storx[x], ' ');
-			storx[x] += 1;
-			mvaddch(story[x], storx[x], 'b');
-			if (storx[x] == COLS && x + 5 >= LEN)
-				seedMap(storx, story, yOff);
-		}
+	{
+		mvaddch(story[x], storx[x], ' ');
+		storx[x] += 1;
+		mvaddch(story[x], storx[x], 'b');
+		if (storx[x] == COLS && x + 5 >= LEN)
+			seedMap(storx, story, yOff);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -64,7 +79,6 @@ int main(int argc, char *argv[])
 	int ch;
 
 	srand(std::time(nullptr));
-
 	initscr();
 	cbreak();
 	keypad(stdscr, TRUE);
@@ -73,29 +87,43 @@ int main(int argc, char *argv[])
 	height = 1;
 	width = 1;
 	starty = (LINES - height) / 2;
-	startx = (COLS - width) / 2;
-	printw("Press F1 to exit, lines: %d, cols: %d\n", LINES, COLS);
+	startx = 30;//(COLS - width) / 2;
 	refresh();
 	my_win = newwin(height, width, starty, startx);
-
-	mvaddch(starty, startx, '^');
 
 	int i = 0;
 	int storx[LEN];
 	int	story[LEN];
-	timeout(20);
-//	wtimeout(my_win, 1);
 	seedMap(storx, story, 0);
 	int storx1[LEN];
 	int	story1[LEN];
-	seedMap(storx1, story1, 50);
-	
-	while((ch = getch()) != KEY_F(1))
+	seedMap(storx1, story1, LINES - 10);
+
+	timeout(20);
+	std::chrono::time_point<std::chrono::system_clock> start, end; 
+
+	start = std::chrono::system_clock::now(); 
+	end = std::chrono::system_clock::now(); 
+
+	std::chrono::duration<double> elapsed_seconds;
+
+
+	while ((ch = getch()) != KEY_F(1))
 	{
-		winRefresh(storx1, story1, 50);
+		winRefresh(storx1, story1, LINES -  10);
 		winRefresh(storx, story, 0);
 		mvaddch(starty, startx, '^');
 		move(starty, startx);
+		end = std::chrono::system_clock::now();
+		start = std::chrono::system_clock::now();
+		elapsed_seconds = end - start;
+		//this tells us how long till we refresh frames
+		while (elapsed_seconds.count() < 0.03f)
+		{
+			elapsed_seconds = end - start;
+			end = std::chrono::system_clock::now();
+		}
+
 		switch(ch)
 		{
 			case KEY_LEFT:
@@ -106,21 +134,21 @@ int main(int argc, char *argv[])
 				}
 				break;
 			case KEY_RIGHT:
-				if (moveOk(starty, startx + 1))
+				if (startx < 20 &&  moveOk(starty, startx + 1))
 				{
 					mvaddch(starty, startx, ' ');
 					startx++;
 				}
 				break;
 			case KEY_UP:
-				if (moveOk(starty - 1, startx))
+				if (moveOk(starty - 11, startx))
 				{
 					mvaddch(starty, startx, ' ');
 					starty--;
 				}
 				break;
 			case KEY_DOWN:
-				if (moveOk(starty + 1, startx))
+				if (moveOk(starty + 11, startx))
 				{
 					mvaddch(starty, startx, ' ');
 					starty++;
@@ -131,6 +159,7 @@ int main(int argc, char *argv[])
 		}
 		i++;
 	}
+
 	endwin();
 	std::cout << "ran loop " + std::to_string(i) + " times\n";
 	return 0;
